5fd1583e00dcec65588b91c3a7cb2b4b68203a06
Gabriel Brousset <gabriel.brousset01@gmail.com>
2023-06-28 13:12:07 -0600

    disabled shuffle when game is active

--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -1,43 +1,46 @@
-import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
-import Chat from "./Chat";
-import TeamsPanel from "./TeamsPanel";
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import Chat from './Chat';
+import TeamsPanel from './TeamsPanel';
 
 export default class RightPanel extends Component {
-	state={
-		message: "",
-	};
-	render(){
-		return(
-			<div className="rightPanel four wide column">
-				<Chat
-					sendMessage={this.props.sendMessage}
-					messages={this.props.messages}
-					user={this.props.user}
-				>
-				</Chat>
-				<TeamsPanel
-					toggleSpymaster={this.props.toggleSpymaster}
-					becomeSpymaster={this.props.becomeSpymaster}
-					blueSpymaster={this.props.blueSpymaster}
-					redSpymaster={this.props.redSpymaster}
-					user={this.props.user}
-					users={this.props.users}
-					joinTeam={this.props.joinTeam}
-					switchTeam={this.props.switchTeam}
-					changeNameInput ={this.props.changeNameInput}
-					game={this.props.game}
-					blueWins={this.props.blueWins}
-					redWins={this.props.redWins}
-					blueCount={this.props.blueCount}
-					redCount={this.props.redCount}
-					gameActive={this.props.gameActive}
-				>
-				</TeamsPanel>
-				<div className="rightPanelButtons">
-					<Button onClick={this.props.shuffleTeams}>Shuffle Teams</Button>
-				</div>
-			</div>
-		);
-	};
+    state = {
+        message: '',
+    };
+    render() {
+        return (
+            <div className='rightPanel four wide column'>
+                <Chat
+                    sendMessage={this.props.sendMessage}
+                    messages={this.props.messages}
+                    user={this.props.user}
+                ></Chat>
+                <TeamsPanel
+                    toggleSpymaster={this.props.toggleSpymaster}
+                    becomeSpymaster={this.props.becomeSpymaster}
+                    blueSpymaster={this.props.blueSpymaster}
+                    redSpymaster={this.props.redSpymaster}
+                    user={this.props.user}
+                    users={this.props.users}
+                    joinTeam={this.props.joinTeam}
+                    switchTeam={this.props.switchTeam}
+                    changeNameInput={this.props.changeNameInput}
+                    game={this.props.game}
+                    blueWins={this.props.blueWins}
+                    redWins={this.props.redWins}
+                    blueCount={this.props.blueCount}
+                    redCount={this.props.redCount}
+                    gameActive={this.props.gameActive}
+                ></TeamsPanel>
+                <div className='rightPanelButtons'>
+                    <Button
+                        disabled={this.props.gameActive}
+                        onClick={this.props.shuffleTeams}
+                    >
+                        Shuffle Teams
+                    </Button>
+                </div>
+            </div>
+        );
+    }
 }
--- src/components/TeamToggleComponent.js
+++ src/components/TeamToggleComponent.js
@@ -1,61 +1,81 @@
-import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
-import TeamComponent from "./TeamComponent"
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import TeamComponent from './TeamComponent';
 
 export default class TeamToggleComponent extends Component {
-	state={
-		isHovering: false,
-	};
-	handleMouseHover = () => {
-		this.setState(this.toggleHoverState)
-	};
-	toggleHoverState = () => {
-		return{
-			isHovering: !this.state.isHovering,
-		};
-	};
-	handleJoinTeam = () => {
-		this.props.joinTeam();
-	};
-	render(){
-		if (!this.props.user.team) {
-			return(
-				<div className="teamContent joinTeam"
-					onMouseLeave={this.handleMouseHover}
-				>
-					<Button  type="submit" size="big" className="ui button" onClick={this.handleJoinTeam}>Join Team</Button>
-				</div>
-			);
-		} else if (this.state.isHovering && !this.props.gameActive && (this.props.user.team!==this.props.team)) {
-			return(
-				<div className="teamContent joinTeam"
-					onMouseLeave={this.handleMouseHover}
-				>
-					<Button  type="submit" size="big" className="ui button" onClick={this.props.switchTeam}>Join Team</Button>
-				</div>
-			);
-		} else {
-			return(
-				<div className="teamContent"
-					onMouseEnter={this.handleMouseHover}
-					onMouseLeave={this.handleMouseHover}
-				>
-					<TeamComponent
-						user = {this.props.user}
-						users = {this.props.users}
-						teamWins={this.props.teamWins}
-						teamUsers={this.props.teamUsers}
-						gameActive={this.props.gameActive}
-						changeNameInput ={this.props.changeNameInput}
-						toggleSpymaster={this.props.toggleSpymaster}
-						teamSpymaster={this.props.teamSpymaster}
-					>
-					</TeamComponent>
-					<div className={"scoreboard " + this.props.team}>
-						<span>{this.props.teamCount}</span>
-					</div>
-				</div>
-			);
-		}
-	}
+    state = {
+        isHovering: false,
+    };
+    handleMouseHover = () => {
+        this.setState(this.toggleHoverState);
+    };
+    toggleHoverState = () => {
+        return {
+            isHovering: !this.state.isHovering,
+        };
+    };
+    handleJoinTeam = () => {
+        this.props.joinTeam();
+    };
+    render() {
+        if (!this.props.user.team) {
+            return (
+                <div
+                    className='teamContent joinTeam'
+                    onMouseLeave={this.handleMouseHover}
+                >
+                    <Button
+                        type='submit'
+                        size='big'
+                        className='ui button'
+                        onClick={this.handleJoinTeam}
+                    >
+                        Join Team
+                    </Button>
+                </div>
+            );
+        } else if (
+            this.state.isHovering &&
+            !this.props.gameActive &&
+            this.props.user.team !== this.props.team
+        ) {
+            return (
+                <div
+                    className='teamContent joinTeam'
+                    onMouseLeave={this.handleMouseHover}
+                >
+                    <Button
+                        type='submit'
+                        size='big'
+                        className='ui button'
+                        onClick={this.props.switchTeam}
+                    >
+                        Join Team
+                    </Button>
+                </div>
+            );
+        } else {
+            return (
+                <div
+                    className='teamContent'
+                    onMouseEnter={this.handleMouseHover}
+                    onMouseLeave={this.handleMouseHover}
+                >
+                    <TeamComponent
+                        user={this.props.user}
+                        users={this.props.users}
+                        teamWins={this.props.teamWins}
+                        teamUsers={this.props.teamUsers}
+                        gameActive={this.props.gameActive}
+                        changeNameInput={this.props.changeNameInput}
+                        toggleSpymaster={this.props.toggleSpymaster}
+                        teamSpymaster={this.props.teamSpymaster}
+                    ></TeamComponent>
+                    <div className={'scoreboard ' + this.props.team}>
+                        <span>{this.props.teamCount}</span>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
