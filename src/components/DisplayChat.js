714630a0366d8082040e0388621d1a73aaeb9b06
Gabriel Brousset <gabriel.brousset01@gmail.com>
2023-06-28 12:57:40 -0600

    split client and server, and some minor fixes

---
+++ src/components/DisplayChat.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+
+export default class DisplayChat extends Component {
+    formatMessage = (message) => {
+        switch (message.type) {
+            default:
+                return (
+                    <div className='messageText' key={uuid.v4()}>
+                        <span className={message.color + 'Count'}>
+                            {message.name}:
+                        </span>{' '}
+                        {message.message}
+                    </div>
+                );
+            case 'spymasterClue':
+                return (
+                    <div
+                        className={'messageClue-' + message.color}
+                        key={uuid.v4()}
+                    >
+                        <span>
+                            {message.message} {message.number}
+                        </span>
+                    </div>
+                );
+            case 'select clue':
+                if (message.isAssassin) {
+                    return (
+                        <div>
+                            <div className='ui clearing divider'></div>
+                            <div
+                                className='messageText messageLog'
+                                key={uuid.v4()}
+                            >
+                                <b>
+                                    <span className={message.color + 'Count'}>
+                                        {message.name}
+                                    </span>{' '}
+                                    <em>clicked</em>{' '}
+                                    <span
+                                        className={message.clueColor + 'Count'}
+                                    >
+                                        {message.message} and lost the game
+                                    </span>
+                                </b>
+                            </div>
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className='messageText messageLog' key={uuid.v4()}>
+                            <span className={message.color + 'Count'}>
+                                {message.name}
+                            </span>{' '}
+                            <em>clicked</em>{' '}
+                            <span className={message.clueColor + 'Count'}>
+                                {message.message}
+                            </span>
+                        </div>
+                    );
+                }
+            case 'end game':
+                return (
+                    <div key={uuid.v4()}>
+                        <div className='ui divider'></div>
+                        <div className='messageText messageLog'>
+                            <b>
+                                <span className={message.color + 'Count'}>
+                                    {message.name}
+                                </span>{' '}
+                                ended the Game
+                            </b>
+                        </div>
+                    </div>
+                );
+            case 'end turn':
+                return (
+                    <div className='messageText messageLog' key={uuid.v4()}>
+                        <span className={message.color + 'Count'}>
+                            {message.name}
+                        </span>
+                        <span> ended its team's turn</span>
+                    </div>
+                );
+        }
+    };
+    render() {
+        let messages = [];
+        if (this.props.messages) {
+            messages = this.props.messages.map((message) =>
+                this.formatMessage(message)
+            );
+        }
+        return <div className='chatWindow'>{messages.reverse()}</div>;
+    }
+}
